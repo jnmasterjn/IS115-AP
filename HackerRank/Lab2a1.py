af8cb42343228566d2e658bbe1bc3e579913b63c
Janice <janice0975751227@gmail.com>
2026-02-06 19:00:35 +0800

    add task

---
+++ HackerRank/Lab2a1.py
@@ -0,0 +1,51 @@
+'''
+Sample Input 0:
+68
+155
+228
+264
+372
+
+11
+33
+120
+146
+155
+180
+200
+256
+264
+286
+
+Sample Output 0:
+
+68,False
+155,True
+228,False
+264,True
+372,False
+'''
+
+def lab2a1(u_inut):
+    for i in (u_inut):
+        print(i)
+
+
+test_input = """68
+155
+228
+264
+372
+
+11
+33
+120
+146
+155
+180
+200
+256
+264
+286"""
+
+print(lab2a1(test_input))
